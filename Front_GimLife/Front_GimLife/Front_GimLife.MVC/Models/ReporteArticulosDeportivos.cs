398bf11fc0e856b25d280c148dd9ec8dc339b1b0
styu23 <stefanirozu15@gmail.com>
2022-07-03 21:21:10 -0500

    v
    vv

---
+++ Front_GimLife/Front_GimLife/Front_GimLife.MVC/Models/ReporteArticulosDeportivos.cs
@@ -0,0 +1,13 @@
+﻿namespace Front_GimLife.MVC.Models
+{
+    public class ReporteArticulosDeportivos
+    {
+        public int id_producto { get; set; }
+        public string nombre { get; set; }
+        public int cantidad { get; set; }
+        public int precio_compra { get; set; }
+        public int precio_venta { get; set; }
+        public int proveedor { get; set; }
+
+    }
+}
